743d3959f7b0269fb5a54de2c405b406eb9812a9
carolina <carolina@sindominio.net>
2013-11-25 11:34:39 +0100

    Modify .gitignore file, modify specs, deleted integration tests examples

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
   validates :name, :event_type,  :day,  presence: true
-  has_and_belongs_to_many :tags
-  belongs_to :location
+  has_and_belongs_to_many :tags,  counter_cache: true
+  belongs_to :location,  counter_cache: true
   belongs_to :organizer
 end
---
+++ spec/models/organizer_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Organizer do
+
+  it "should name be present" do
+  @organizer = Organizer.new(:name => "OTS")  
+  @organizer.name.should be_present
+  end
+  
+  it 'should be invalid if no name is present' do
+  @organizer = Organizer.new
+  @organizer.should_not be_valid
+
+  end  
+
+
+end
