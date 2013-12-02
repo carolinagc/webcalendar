dc994023e6afe35d2435bd2a566e49208b0b9e75
carolina <carolina@sindominio.net>
2013-12-02 23:45:50 +0100

    Add error message to user views

---
+++ spec/integration/users_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+feature 'User' do
+  scenario 'Create account' do
+    fill_in_name_email
+    fill_in 'user_password', with: 'foobar'
+    fill_in 'user_password_confirmation', with: 'foobar'
+    expect { click_button('Create account') }.to change(User, :count).by(1)
+  end
+
+  scenario 'passwords mismatch'  do
+    fill_in_name_email
+    fill_in 'user_password', with: 'foobar'
+    fill_in 'user_password_confirmation', with: 'foolala'
+    expect { click_button('Create account') }.to change(User, :count).by(0)
+  end
+
+  scenario 'password should not be blank' do
+    fill_in_name_email
+    fill_in 'user_password', with: ''
+    fill_in 'user_password_confirmation', with: ''
+    expect { click_button('Create account') }.to change(User, :count).by(0)
+  end
+
+  scenario 'password length has to be at least 6 characters' do
+    fill_in_name_email
+    fill_in 'user_password', with: 'foo'
+    fill_in 'user_password_confirmation', with: 'foo'
+    expect { click_button('Create account') }.to change(User, :count).by(0)
+  end
+
+  def fill_in_name_email 
+    visit new_user_path
+    expect(page).to have_content('Name')
+    fill_in 'user_name', with: 'julia'
+    fill_in 'user_email', with: 'julia@lala.com'
+   
+  end
+
+end
