6e0c18273056eda31f43f311567043d199d2424b
carolina <carolina@sindominio.net>
2013-12-02 18:17:45 +0100

    Delete files since they were wrongly placed or named

--- spec/integration/view_homepage_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-feature 'view the homepage' do
-  scenario 'List of events' do
-    visit root_path
-    expect(page).to have_css 'title', text: 'List of events'
-  end
-end
