c6e23d242203298540d32a99cc7b9ef2642d2f5b
carolina <carolina@sindominio.net>
2013-12-04 17:22:34 +0100

    Add sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-#    user = User.where(email: params[:session][:email].downcase)
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.where(email: params[:session][:email].downcase)
+#    user = User.find_by(email: params[:session][:email].downcase)
 
     if user && user.authenticate(params[:session][:password])
       redirect_to user_path(user)
@@ -16,5 +16,8 @@ class SessionsController < ApplicationController
  end
 
   def destroy
+    sign_out
+    redirect_to root_url
+
   end
 end
