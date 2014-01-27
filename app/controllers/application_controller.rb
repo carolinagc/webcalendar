20ad7c674752db0dab60f5617b25f5bad1e5b789
carolina <carolina@sindominio.net>
2014-01-27 18:49:03 +0100

    Add internationalization, add conf to routes, application.rb and application_controller, all view modified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   after_filter :flash_to_headers
-
+  before_action :set_locale
+ 
   include SessionsHelper
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+      def default_url_options(options={})
+        logger.debug "default_url_options is passed options: #{options.inspect}\n"
+        { locale: I18n.locale }
+      end
 
   def sign_in_as (email)
     
@@ -32,8 +42,5 @@ class ApplicationController < ActionController::Base
 	        return type unless flash[type].blank?
 	    end
 	end
-
-
-
 
 end
