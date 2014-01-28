5c48c6af2418558cb141046277ee694677494ba6
Martin Meyerhoff <mamhoff@googlemail.com>
2014-01-28 22:15:46 +0100

    remove classic sign in routes and rename paths in tests accordingly

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Webcalendar::Application.routes.draw do
   resources :events
   resources :locations
   resources :organizers
+
+  match '/signup', to: 'users#new', via:'get'
+
   devise_for :users
   resources :users
-  match '/signup', to: 'users#new', via:'get'
-  match '/signin', to: 'devise/sessions#new', via: 'get'
-  match '/signout', to: 'devise/sessions#new', via:'delete'
+
   resources :tags, except: [:show]
 
   match '/calendar', to: 'events#calendar', via:'get'
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 feature "AuthenticationPages" do
   scenario 'signin page' do
-    visit signin_path
+    visit new_user_session_path
     expect(page).to have_content("Sign in")
   end
   scenario 'with invalid information' do
-    visit signin_path
+    visit new_user_session_path
     expect(page).to have_content("Sign in")
     expect(page).to have_selector("div.alert.alert-error", :value => "Invalid")
   end
   scenario 'with valid information' do
     @user = User.create(:name => 'julia', :email => 'julia@lala.com', :password=> 'foolala', :password_confirmation => 'foolala')
-    visit signin_path
+    visit new_user_session_path
     fill_in 'session_email', with: 'julia@lala.com'
     fill_in 'session_password', with: 'foolala'
     click_button 'Sign in'
