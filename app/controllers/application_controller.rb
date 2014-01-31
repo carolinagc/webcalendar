68e2deddb0a0f0c410db0ff76444175607e2a3c7
Martin Meyerhoff <mamhoff@googlemail.com>
2014-01-31 12:57:35 +0100

    remove classic sessions helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,6 @@ class ApplicationController < ActionController::Base
 
   before_action :set_locale
  
-  include SessionsHelper
-
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140123125130) do
+ActiveRecord::Schema.define(version: 20140128164140) do
 
   create_table "events", force: true do |t|
     t.string   "name"
