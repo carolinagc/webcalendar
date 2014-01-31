062b0072e5c51207bec8b03c95b82831e75754d3
Martin Meyerhoff <mamhoff@googlemail.com>
2014-01-31 13:04:55 +0100

    removed strong params for user model - handled by devise now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,4 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-
-private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-
 end
