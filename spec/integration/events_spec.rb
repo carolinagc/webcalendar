fb1023d65c73c0be63c1eaa93d5b27a92c09e018
carolina <carolina@sindominio.net>
2014-01-31 12:12:17 +0100

    Add tests to check calendar events view

--- spec/integration/events_spec.rb
+++ spec/integration/events_spec.rb
@@ -61,6 +61,28 @@ feature 'events' do
     find('select', :text => "Betahaus").click 
 
   end
+  scenario 'Monthly calendar view' do
+    visit root_path 
+    expect(page).to have_content('Week')
+    expect(page).to have_content(Date.today.strftime("%B")) #month in words
+    expect(page).to have_css("td.notmonth")
+    expect(page).to have_link(@event.name, href: event_path(@event))
+  end
+
+  scenario 'Show one event coming from calendar view' do
+    visit root_path 
+    visit event_path @event.id
+    expect(page).to have_content('Printing the washing machine')
+    expect(page).to have_content('Workshop')
+    expect(page).to have_content('Duration')
+    find("#backIcon").click
+    expect(page).to have_content("Week")
+  end
+
+  scenario 'Show week view' do
+    visit root_path 
+    click_link("Week")
+  end
 
 
 end
