e4a84c7c601771efd0c908ef2806450b08f678d6
Philipp Hansch <dev@phansch.net>
2014-02-04 23:20:20 +0100

    Use factories for organizer model spec

--- spec/models/organizer_spec.rb
+++ spec/models/organizer_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
 describe Organizer do
-
-  it "should name be present" do
-  @organizer = Organizer.new(:name => "OTS")  
-  @organizer.name.should be_present
+  before do
+    @organizer = FactoryGirl.build(:organizer)
   end
-  
+
+  it "should have a name" do
+    expect(@organizer.name).to be_present
+  end
+
   it 'should be invalid if no name is present' do
-    @organizer = Organizer.new
-    @organizer.should_not be_valid
-  end  
+    expect(Organizer.new).to be_invalid
+  end
 
   it { should respond_to(:url) }
-
 end
