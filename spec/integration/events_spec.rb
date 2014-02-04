e11081a7a3751fda928fd774d7468ad99cb25e96
carolina <carolina@sindominio.net>
2014-02-04 12:18:55 +0100

    Add test for public/private events

--- spec/integration/events_spec.rb
+++ spec/integration/events_spec.rb
@@ -110,6 +110,11 @@ feature 'events' do
   end
 
   scenario 'If user signed_in show all events' do
+    visit root_path
+    click_link(I18n.translate! :sign_in)
+    fill_in 'user_email', with: "lala@lala.com"
+    fill_in 'user_password', with: "123456789"
+    click_button I18n.translate! :sign_in
 
 
   end
