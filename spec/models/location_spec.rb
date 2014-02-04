eefef079a2c6c674ef4d71f9fc8f274103a064fa
Philipp Hansch <dev@phansch.net>
2014-02-04 23:35:27 +0100

    Use factories for location model spec

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,32 +1,31 @@
 require 'spec_helper'
 
 describe Location do
-  before :each do
-    @location = Location.new(:name =>"Betahaus", :address => "Prinzessinnenstrassee 19-20")
+  before :all do
+    @location = FactoryGirl.build(:location)
   end
 
+  it { expect(@location).to respond_to(:url) }
+
   it 'should have a name' do
-    expect(@location).to be_present
+    expect(@location.name).to be_present
   end
 
   it 'should be invalid if it has no name' do
-    @location = Location.new
-    expect(@location).to_not be_valid
+    expect(Location.new).to be_invalid
   end
 
   it 'should have an address' do
     expect(@location.address).to be_present
   end
 
-  it { should respond_to(:url) }
+  it 'should include events' do
+    event1 = FactoryGirl.build(:event, name: "Event1",
+                               startdatetime: Date.today)
+    event2 = FactoryGirl.build(:event, name: "Event2",
+                               startdatetime: Date.today + 1)
+    @location.events = [event1, event2]
 
-  it 'includes events' do
-    event1 = Event.new(:name => 'Event1', :startdatetime => Date.today)
-    event2 = Event.new(:name => 'Event2', :startdatetime => Date.today+1)
-    location = Location.new(:name => 'lala', events: [event1, event2])
-#    location = Location.new(:name => 'lala', events)
-    location.events.should include(event1)
-    location.should have(2).events
+    expect(@location.events).to include(event1, event2)
   end
-
 end
