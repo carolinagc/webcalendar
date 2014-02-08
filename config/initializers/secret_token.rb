c0c51f5a823cfedd987c544b95eecfd5eefe89ee
Philipp Hansch <dev@phansch.net>
2014-02-08 23:13:16 +0100

    Fix typo

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,2 +1,2 @@
-secret = Rails.env.production? ? EVN['SECRET_TOKEN'] : ('x' * 30)
+secret = Rails.env.production? ? ENV['SECRET_TOKEN'] : ('x' * 30)
 Webcalendar::Application.config.secret_key_base = secret
